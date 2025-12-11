9b6ff0d541f7108df765622d0aa28d0a8caef26f
Tim <maddestfanat@yandex.ru>
2025-12-12 02:40:02 +0300

    Fix codestyle

--- tests/__pycache__/unittests.py
+++ tests/__pycache__/unittests.py
@@ -2,26 +2,25 @@ import unittest
 import rectangle  
 
 class RectangleTestCase(unittest.TestCase):
-    
-    def test_zero_mul(self):
-        """Проверка площади с нулевым значением."""
-        res = rectangle.area(10, 10)
-        self.assertEqual(res, 0)
+    def test_area_zero(self):
+        """Тест площади прямоугольника со стороной 0"""
+        result = rectangle.area(0, 10)
+        self.assertEqual(result, 0)
 
-    def test_square_mul(self):
-        """Проверка площади квадрата (10x10)."""
-        res = rectangle.area(10, 10)
-        self.assertEqual(res, 100)
-    
-    def test_perimeter_calculation(self):
-        """Проверка правильности вычисления периметра."""
-        res = rectangle.perimeter(10, 5)
-        self.assertEqual(res, 30)
+    def test_area_square(self):
+        """Тест площади квадрата 10x10"""
+        result = rectangle.area(10, 10)
+        self.assertEqual(result, 100)
 
     def test_perimeter_zero(self):
-        """Проверка периметра, если одна сторона равна 0."""
-        res = rectangle.perimeter(10, 0)
-        self.assertEqual(res, 20)
+        """Тест периметра прямоугольника 10x0"""
+        result = rectangle.perimeter(10, 0)
+        self.assertEqual(result, 20)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_perimeter(self):
+        """Тест периметра прямоугольника 10x5"""
+        result = rectangle.perimeter(10, 5)
+        self.assertEqual(result, 30)
+
+
+unittest.main()
