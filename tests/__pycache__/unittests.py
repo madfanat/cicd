fb93c88961adc2bb8851a7b936b75c0d21658ea4
Tim <maddestfanat@yandex.ru>
2025-12-12 03:11:47 +0300

    Extend functionality

--- tests/__pycache__/unittests.py
+++ tests/__pycache__/unittests.py
@@ -6,7 +6,17 @@ class RectangleTestCase(unittest.TestCase):
         """Тест площади прямоугольника со стороной 0"""
         result = rectangle.area(0, 10)
         self.assertEqual(result, 0)
-
+    
+    def test_area_negative(self):
+        """Тест площади прямоугольника с отрицательной стороной"""
+        result = rectangle.area(-10, 10)
+        self.assertEqual(result, -100)
+    
+    def test_area_broken(self):
+        """Тест неправильных входных данных"""
+        with self.assertRaises(TypeError):
+            rectangle.area("string1", "string2")
+        
     def test_area_square(self):
         """Тест площади квадрата 10x10"""
         result = rectangle.area(10, 10)
@@ -16,6 +26,11 @@ class RectangleTestCase(unittest.TestCase):
         """Тест периметра прямоугольника 10x0"""
         result = rectangle.perimeter(10, 0)
         self.assertEqual(result, 20)
+    
+    def test_perimeter_negative(self):
+        """Тест периметра прямоугольника с отрицательными сторонами"""
+        result = rectangle.perimeter(-10, -5)
+        self.assertEqual(result, -30)
 
     def test_perimeter(self):
         """Тест периметра прямоугольника 10x5"""
